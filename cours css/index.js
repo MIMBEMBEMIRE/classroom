8738b183ac0cc0c478312c60a780d01d602bfe18
unknown <miramimbembe1991@gmail.com>
2022-02-27 10:41:59 +0530

    faire un click sur un button pour faire apparaitre le tex

--- cours css/index.js
+++ cours css/index.js
@@ -1,4 +1,17 @@
 const yell=document.getElementById("col");
+const button1=document.getElementById("bnt1");
+const button2=document.getElementById("bnt2");
+const reponses=document.getElementById("reponse");
 yell.addEventListener("click",() => {
-    yell.classList.add("yell1");
+    yell.classList.toggle("yell1");
+
+});
+button1.addEventListener("click",() => {
+reponses.style.visibility = "visible";
+reponses.style.background="green";
+
+});
+button2.addEventListener("click",() =>{
+    reponses.style.visibility ="visible";
+    reponses.style.background="yellow";
 });
